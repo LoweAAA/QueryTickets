f28c0df9de8db497f1807203d4164cc752055706
Lowe__ <947875760@qq.com>
2017-12-11 21:04:59 +0800

    跨域支持

--- src/main/java/com/saltyfish/querytickets/controller/LoginController.java
+++ src/main/java/com/saltyfish/querytickets/controller/LoginController.java
@@ -5,6 +5,7 @@ import com.saltyfish.querytickets.model.LoginEntity;
 import com.saltyfish.querytickets.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class LoginController {
 
     @Autowired
